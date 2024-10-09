afddcfc9aab033b06c2f7c581a3300ac06821e3f
jkurkure <117855264+jkurkure@users.noreply.github.com>
2024-10-09 08:31:19 +0100

    Update ports.py

--- ports.py
+++ ports.py
@@ -1,6 +1,7 @@
 import sys
 
 SECRET_KEY = "We lay, my love and I, beneath the weeping willow." # Change for your local copy
+PORT_MAX = 2**16 - 1
 
 simple_hasher = lambda s: sum([ord(c) for c in s])
 easy_hasher = lambda s: eval('*'.join([str(ord(c)) for c in s]))
@@ -9,9 +10,9 @@ repeats = lambda n:True in [f"{n}".count(f'{d}') > 2 for d in range(0, 10)]
 hash = simple_hasher(SECRET_KEY.join(sys.argv)) * easy_hasher(SECRET_KEY.join(sys.argv))
 
 if "--nopattern" in sys.argv:
-    allowed = [i for i in range(7700, 65536) if not repeats(i)]
+    allowed = [i for i in range(7700, PORT_MAX+1) if not repeats(i)]
     print(len(allowed))
     print(allowed[hash % len(allowed)])
     sys.exit(0)
 
-print(hash % (65535 - 7700) + 7700)
+print(hash % (PORT_MAX - 6600) + 6600)
